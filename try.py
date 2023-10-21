f68e82d9e8fdb422ca0989aaa1cb1b4184c7d9aa
Malak nasser <mallaknasser812@gmail.com>
2023-10-21 14:28:25 +0300

    Update try.py

--- try.py
+++ try.py
@@ -116,7 +116,6 @@ class MainWindow(QtWidgets.QMainWindow):
             self.phase += 90
 
         self.signaldict[self.name] = self.magnitude, self.frequency, self.phase
-        self.sum_signals_combobox.addItem(self.name)
         self.sinusoidal = self.signalParameters(
             self.magnitude, self.frequency, self.phase)
         self.plot_sin_signal(self.canvas1, self.show_signal_graph,
@@ -129,6 +128,7 @@ class MainWindow(QtWidgets.QMainWindow):
     def display_summed_sin_signals(self):
         
         self.signal_sum += self.sinusoidal
+        self.sum_signals_combobox.addItem(self.name)
         self.plot_sin_signal(self.canvas2, self.summation_graph,
                         self.layout2, self.signal_sum)
         
@@ -158,6 +158,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.x_data = self.time
         self.y_data = self.signal_sum
         self.plot_sin_signal(self.canvas3, self.sampled_graph, self.layout3, self.signal_sum)
+        self.sampler_tab.setCurrentIndex() 
 
 
 
